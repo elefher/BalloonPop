a079c5310365666c3293ee304520c3295ec7236a
elefher <eldelielev@gmail.com>
2018-12-16 14:59:25 +0200

    first commit of forked baby-pop, project name changed to BalloonPop

--- js/balloon.js
+++ js/balloon.js
@@ -3,20 +3,41 @@ class Balloon extends Phaser.GameObjects.Sprite {
 		super(scene, x,y, key);
 		scene.add.existing(this);
 		this.setInteractive();
-		this.on('pointerdown', e => this.onPop(e));
+		this.blowBallon = false;
+		this.scaleVal = 1;
+		this.scaleMax = 2;
+
+		this.on('pointerdown', e => {
+			this.blowBallon = true;
+		});
+
+		this.on('pointerup', e => {
+			this.blowBallon = false;
+			this.scene.getDuration(e);
+		});
 
 		this.angle = -15 + Math.random()*30;				// -15 to 15
 		this.angleDir = -1 + Math.round(Math.random())*2;	// 1 or -1
 	}
 
-	onPop(event) {
-		// console.log('onPop()\t this=%o, event=%o', this, event);
-		this.scene.addBalloon();
-		this.scene.killBalloon(this);
-	}
+	// update(time,delta) {
+	// 	if(this.y<-64) return this.scene.gameOver();
+	//
+	// 	this.x += this.angle/20;
+	// 	if(this.x<64) this.x = 64;
+	// 	if(this.x>(this.scene.sys.game.config.width-64)) this.x = this.scene.sys.game.config.width-64;
+	//
+	// 	this.angle += this.angleDir/5;
+	// 	if(this.angle>25 || this.angle<-25) this.angleDir *= -1;
+	//
+	// 	this.y -= this.speed;
+	// }
 
 	update(time,delta) {
-		if(this.y<-64) return this.scene.gameOver();
+		if(this.blowBallon){
+			this.scaleVal += 0.005;
+			this.scene.blowBallon(this, this.scaleVal);
+		}
 
 		this.x += this.angle/20;
 		if(this.x<64) this.x = 64;
@@ -25,6 +46,6 @@ class Balloon extends Phaser.GameObjects.Sprite {
 		this.angle += this.angleDir/5;
 		if(this.angle>25 || this.angle<-25) this.angleDir *= -1;
 
-		this.y -= this.speed;
+		this.y = this.scene.sys.game.config.height / 2;
 	}
 }
--- js/playScene.js
+++ js/playScene.js
@@ -1,27 +1,28 @@
 class PlayScene extends Phaser.Scene {
 	constructor() {
-		super({key:'play', active:true});
+		super({key: 'play', active: true});
 	}
 
 	preload() {
-		this.load.image('sky',				'assets/img/sky.png');
-		this.load.image('balloon-red',		'assets/img/balloon-red.png');
-		this.load.image('balloon-blue',		'assets/img/balloon-blue.png');
-		this.load.image('balloon-yellow',	'assets/img/balloon-yellow.png');
-		this.load.image('balloon-green',	'assets/img/balloon-green.png');
-		this.load.image('balloon-purple',	'assets/img/balloon-purple.png');
-		this.load.audio('pop',				'assets/audio/pop.mp3');
+		this.load.image('sky', 'assets/img/skyCloud.png');
+		// this.load.image('balloon-red', 'assets/img/balloon-red.png');
+		// this.load.image('balloon-blue', 'assets/img/balloon-blue.png');
+		this.load.image('balloon-yellow', 'assets/img/balloon-yellow.png');
+		// this.load.image('balloon-green', 'assets/img/balloon-green.png');
+		// this.load.image('balloon-purple', 'assets/img/balloon-purple.png');
+		this.load.audio('pop', 'assets/audio/pop.mp3');
 	}
 
 	create() {
 		this.sky = this.add.image(0, 0, 'sky');
-		this.sky.setOrigin(0,0);
-		this.sky.setScale(3,3);
+		this.sky.setOrigin(0, 0);
+		this.sky.setScale(3, 3);
 
 		this.balloons = [];
-		this.score = 0;
+		this.clickDuration = 0;
 
-		this.scoreText = this.add.text(50,50, '', {fontFamily:'Arial Black', fontSize:74, color:'#c51b7d'});
+
+		this.scoreText = this.add.text(50, 50, '', {fontFamily: 'Arial Black', fontSize: 74, color: '#c51b7d'});
 		this.scoreText.setStroke('#de77ae', 5);
 		this.scoreText.setShadow(2, 2, '#333333', 2, true, false);
 
@@ -30,37 +31,53 @@ class PlayScene extends Phaser.Scene {
 		this.startGame();
 	}
 
+	getDuration(pointer) {
+		this.clickDuration = Math.round(pointer.upTime - pointer.downTime);
+	}
+
 	startGame() {
-		var sx = (this.sys.game.config.width)/3;
-		for(var i=0;i<3;i++) {
-			this.addBalloon(sx*i + sx*0.5);
-		}
-		this.score = 0;
+		var sx = (this.sys.game.config.width) / 3;
+		this.addBalloon((this.sys.game.config.width) / 2, this.sys.game.config.height);
 	}
 
 	addBalloon(x) {
-		if(!x) x = Math.floor(Math.random()*(this.sys.game.config.width-128)) + 64;
-		var balloon = new Balloon(this, x, this.sys.game.config.height+128, this.randomColor());
-		balloon.speed = 0.25 + Math.random() + (this.score/10);
+		if (!x) x = Math.floor(Math.random() * (this.sys.game.config.width - 128)) + 64;
+		var balloon = new Balloon(this, x, this.sys.game.config.height + 128, 'balloon-yellow');
+		balloon.speed = 0.25 + Math.random() + (this.score / 10);
 		this.balloons.push(balloon);
-		this.score++;
 	}
+
+	blowBallon(balloon, scaleVal) {
+		if(!this.ballonHealthIsGood(balloon, scaleVal)){
+			this.killBalloon(balloon);
+		}
+
+		balloon.setScale(scaleVal, scaleVal);
+	}
+
+	ballonHealthIsGood(balloon, scaleVal){
+		return scaleVal > balloon.scaleMax ? false : true
+	}
+
+	// initBalloon(balloon){
+	// 	var tweenInitBalloon = this.tweens.add({
+	// 		targets: balloon,
+	// 		scaleX: 1,
+	// 		scaleY: 1,
+	// 		duration: 2000,
+	// 		delay: 500,
+	// 	});
+	// }
 
 	killBalloon(balloon) {
 		this.popSound.play();
-		this.balloons = this.balloons.filter(b => b!==balloon);
-		var tween1 = this.tweens.add({
-			targets: balloon,
-			scaleX: 1.5,
-			scaleY: 1.5,
-			duration: 50
-		});
-		var tween2 = this.tweens.add({
+		this.balloons = this.balloons.filter(b => b !== balloon);
+		var tweenDestroyBalloon = this.tweens.add({
 			targets: balloon,
 			scaleX: 0,
 			scaleY: 0,
-			duration: 50,
-			delay: 50,
+			duration: 150,
+			delay: 100,
 			onComplete: () => balloon.destroy()
 		});
 	}
@@ -72,15 +89,8 @@ class PlayScene extends Phaser.Scene {
 		this.startGame();
 	}
 
-	randomColor() {
-		var colors = ['balloon-red','balloon-blue','balloon-yellow','balloon-green','balloon-purple'];
-		var random = Math.floor(Math.random()*colors.length);
-		return colors[random];
-	}
-
-	update(time,delta) {
-		// console.log('update time=%o, delta=%o', time,delta);
-		this.balloons.forEach(b => b.update(time,delta));
-		this.scoreText.setText('Score: '+this.score);
+	update(time, delta) {
+		this.balloons.forEach(b => b.update(time, delta));
+		this.scoreText.setText(' Click Duration: ' + this.clickDuration);
 	}
 }
